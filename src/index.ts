06d5952c4ccedb830186d626622a39dcaf81cc1e
mart8ins <mart8ins@gmail.com>
2022-05-11 09:40:37 +0300

    finished structure for future projects, added Slist

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,50 @@
 require("dotenv").config();
 
 import express, { Request, Response, NextFunction } from "express";
-import { sListRoute } from "./projects/SHOPPING-LIST/shopping-list-routes";
+import sListRoute from "./projects/SHOPPING-LIST/shopping-list-routes";
 import cors from "cors";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
 const port = process.env.PORT || 3001;
+// let DBName;
 const app = express();
 app.use(express.urlencoded({ extended: true }), express.json());
 app.use(cors());
 
+// app.use((req, res, next) => {
+//     const url = req.originalUrl.slice(1, 6);
+//     console.log(url);
+//     if (url === "sList") {
+//         DBName = "shopping";
+//     }
+//     next();
+// });
+
+// mongoose.connect(
+//     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@shopping.8edhp.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+//     {}
+// );
+
 // PROJECTS
-app.use("/shopping-list", sListRoute);
+app.use("/sList", sListRoute);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ message: err.message });
 });
-
-mongoose
-    .connect(
-        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@shopping.8edhp.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
-        {}
-    )
-    .then(() => {
-        app.listen(port, () => {
-            console.log("App startet on port " + port);
-        });
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+// process.env.DATABASE_NAME
+// mongoose
+//     .connect(
+//         `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@shopping.8edhp.mongodb.net/${DBName}?retryWrites=true&w=majority`,
+//         {}
+//     )
+//     .then(() => {
+//         app.listen(port, () => {
+//             console.log("App startet on port " + port);
+//         });
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+app.listen(port, () => {
+    console.log("App startet on port " + port);
+});
--- src/projects/SHOPPING-LIST/shopping-list-routes.ts
+++ src/projects/SHOPPING-LIST/shopping-list-routes.ts
@@ -1,4 +1,10 @@
 import { Router } from "express";
+import mongoose from "mongoose";
+
+mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@shopping.8edhp.mongodb.net/${process.env.DATABASE_NAME_SLIST}?retryWrites=true&w=majority`,
+    {}
+);
 
 import { createRecipe, getRecipes, deleteRecipe } from "./controllers/recipes";
 import { getGroceryNames, saveGroceryNames } from "./controllers/groceries";
@@ -25,6 +31,4 @@ router.post("/shoppingList/delete", deleteShoppingList);
 // USER
 router.post("/user", signIn);
 
-export const sListRoute = () => {
-    return router;
-};
+export default router;
